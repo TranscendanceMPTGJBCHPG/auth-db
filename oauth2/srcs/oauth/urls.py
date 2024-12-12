fd12e1e6d2ca1bf5c8e3336885d752caf3bd65d8
pgouasmi <pgouasmi@student.42lyon.fr>
2024-12-12 17:27:11 +0100

    [FIX] rmed cli token

--- oauth2/srcs/oauth/urls.py
+++ oauth2/srcs/oauth/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .auth_views import oauth_login, verify_2fa, authfortytwo
-from .get_token_views import gettoken, get_guest_token
+from .get_token_views import get_guest_token
 from .user_data_views import get_user_counters, increment_user_win_counter, increment_user_goal_counter, reset
 
 urlpatterns = [
@@ -9,7 +9,6 @@ urlpatterns = [
     path('authfortytwo/', authfortytwo, name='authfortytwo'),
     path('2fa/', verify_2fa, name='2fa'),
     ######################### GET TOKEN #########################
-    path('gettoken/', gettoken, name='gettoken'),
     path('getguesttoken/', get_guest_token, name='getguesttoken'),
     ######################### USER DATA #########################
     path('getusercounters/', get_user_counters, name='getusercounters'),
